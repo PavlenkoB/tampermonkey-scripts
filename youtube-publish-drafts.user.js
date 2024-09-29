5795b44beb8fdfd41f886dd91a1894cdd01e3be2
Bohdan_pavlenko <godexxxx@gmail.com>
2024-09-29 09:43:46 +0300

    fix naming 3

--- youtube-publish-drafts.user.js
+++ youtube-publish-drafts.user.js
@@ -6,8 +6,8 @@
 // @author       PavlenkoB
 // @match        https://studio.youtube.com/channel/*/videos/upload*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @updateURL    https://github.com/PavlenkoB/tampermonkey-scripts/blob/main/youtube-publish-drafts.user.js
-// @downloadURL  https://github.com/PavlenkoB/tampermonkey-scripts/blob/main/youtube-publish-drafts.user.js
+// @updateURL    https://raw.githubusercontent.com/PavlenkoB/tampermonkey-scripts/refs/heads/main/youtube-publish-drafts.user.js
+// @downloadURL  https://raw.githubusercontent.com/PavlenkoB/tampermonkey-scripts/refs/heads/main/youtube-publish-drafts.user.js
 // @grant        none
 // ==/UserScript==
 
